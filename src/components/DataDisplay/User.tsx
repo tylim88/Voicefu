9f6bb4279c6670a50744c775f655dace1d5d275f
tylim <limteikyee@gmail.com>
2023-05-01 19:53:16 +0800

    remove unused button

--- src/components/DataDisplay/User.tsx
+++ src/components/DataDisplay/User.tsx
@@ -1,12 +1,4 @@
-import {
-    UnstyledButton,
-    Group,
-    Avatar,
-    Text,
-    Box,
-    useMantineTheme,
-    rem,
-} from '@mantine/core'
+import { Group, Avatar, Text, Box, useMantineTheme, rem } from '@mantine/core'
 import { useUserStore } from '~/stores'
 
 export function User() {
@@ -16,7 +8,10 @@ export function User() {
     return user ? (
         <Box
             sx={{
-                paddingTop: theme.spacing.sm,
+                display: 'block',
+                width: '100%',
+                padding: theme.spacing.xs,
+                borderRadius: theme.radius.sm,
                 borderTop: `${rem(1)} solid ${
                     theme.colorScheme === 'dark'
                         ? theme.colors.dark[4]
@@ -24,37 +19,17 @@ export function User() {
                 }`,
             }}
         >
-            <UnstyledButton
-                sx={{
-                    display: 'block',
-                    width: '100%',
-                    padding: theme.spacing.xs,
-                    borderRadius: theme.radius.sm,
-                    color:
-                        theme.colorScheme === 'dark'
-                            ? theme.colors.dark[0]
-                            : theme.black,
-
-                    '&:hover': {
-                        backgroundColor:
-                            theme.colorScheme === 'dark'
-                                ? theme.colors.dark[6]
-                                : theme.colors.gray[0],
-                    },
-                }}
-            >
-                <Group>
-                    <Avatar src={user.photoURL} radius='xl' />
-                    <Box sx={{ flex: 1 }}>
-                        <Text size='sm' weight={500}>
-                            {user.displayName}
-                        </Text>
-                        <Text color='dimmed' size='xs'>
-                            {user.email}
-                        </Text>
-                    </Box>
-                </Group>
-            </UnstyledButton>
+            <Group>
+                <Avatar src={user.photoURL} radius='xl' />
+                <Box sx={{ flex: 1 }}>
+                    <Text size='sm' weight={500}>
+                        {user.displayName}
+                    </Text>
+                    <Text color='dimmed' size='xs'>
+                        {user.email}
+                    </Text>
+                </Box>
+            </Group>
         </Box>
     ) : null
 }
